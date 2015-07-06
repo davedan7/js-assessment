4431e015b35322d64aae572fc8a189b6f7d3c0ef
David Daniel <davedan7@gmail.com>
2015-07-06 08:47:29 -0600

    Objects, some recursion

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,22 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
+    var ret = [];
 
+    for (var property in obj) {
+      if (obj.hasOwnProperty(property)){ // Why is this necessary?
+        ret.push(property + ": " + obj[property])
+      }
+    }
+    return ret;
   }
+
 };
