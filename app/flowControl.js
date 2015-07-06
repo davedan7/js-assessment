0b0e74b56582ce093d5b739047a27b9693d6a89c
David Daniel <davedan7@gmail.com>
2015-07-05 21:12:58 -0600

    Flow Control and functions

--- app/flowControl.js
+++ app/flowControl.js
@@ -3,6 +3,23 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.flowControlAnswers = {
   fizzBuzz : function(num) {
     // write a function that receives a number as its argument;
+    if (typeof num !== 'number') {
+      return false;
+    }
+
+    if (num % 3 === 0 && num % 5 === 0) {
+      return "fizzbuzz"
+    }
+
+    if (num % 3 === 0) {
+      return 'fizz'
+    }
+
+    if (num % 5 === 0 ) {
+      return 'buzz'
+    }
+
+    return num;
     // if the number is divisible by 3, the function should return 'fizz';
     // if the number is divisible by 5, the function should return 'buzz';
     // if the number is divisible by 3 and 5, the function should return
--- app/functions.js
+++ app/functions.js
@@ -2,27 +2,61 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray : function(fn, arr) {
-
+    return fn.apply(null, arr);
   },
 
-  speak : function(fn, obj) {
+  // argsAsArray : function(fn, arr) {
+  //   return fn(arr[0], arr[1], arr[2]);
+  // },
 
+  speak : function(fn, obj) {
+    return fn.call(obj);
   },
 
   functionFunction : function(str) {
-
+    return function (argument) {
+      return str + ', ' + argument;
+    }
   },
 
   makeClosures : function(arr, fn) {
+    // result = [];
 
+    var makeFunction = function (value) {
+      return function () {
+        return fn(value);
+      };
+    }
+
+    var result =  arr.map(function (x) {
+      return makeFunction(x);
+    });
+
+    return result;
+    // for (var i = 0; i < arr.length; i++) {
+    //   result.push(makeFunction(arr[i]));
+    // }
+    // return result;
   },
 
   partial : function(fn, str1, str2) {
-
+    return function (str3) {
+      return fn.call(null, str1, str2, str3);
+    }
   },
 
   useArguments : function() {
+    result = 0;
 
+    for (var i = 0; i < arguments.length; i++) {
+      result += arguments[i];
+    };
+
+    // var result = [].reduce.call(arguments, function (sum, x) {
+    //    return sum + x;
+    // };
+
+    return result;
   },
 
   callIt : function(fn) {
